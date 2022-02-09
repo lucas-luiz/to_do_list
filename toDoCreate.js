a7d22cd613acf87a20fcb891136d6fee8af3628f
lucas-luiz <lucas.ldsilva10@gmail.com>
2022-02-09 15:31:37 -0300

    .

--- toDoCreate.js
+++
@@ -1,6 +0,0 @@
-/* const createButton = document.querySelector(".c-main__new-task")
-const main = document.querySelector(".l-main")
-const toDoList = document.querySelector(".c-to-do-list")
-const inputText = document.querySelector(".c-to-do__input") */
-
-
--- toDoRender.js
+++ toDoRender.js
@@ -1,15 +1,15 @@
-
-
-
 const input = document.querySelector(".c-to-do__input")
 const listHtml = document.querySelector(".c-to-do-list")
 const newTask = document.querySelector(".c-main__new-task")
 const main = document.querySelector(".c-to_do__main")
 const scope = document.querySelector(".c-to_do__scope")
+const regex = `[a-zA-Z0-9 ]+`
 
 
 //CHECK, MOSTRAT DEL BUTTON
 checkboxVerify = function(id, content, element){
+    
+    console.log(id,content,element)
     if(element.checked){
         arrTasks.map(e =>{
             if(e.def == content){
@@ -17,6 +17,7 @@ checkboxVerify = function(id, content, element){
                 localStorage.list = JSON.stringify(arrTasks)
             }
         })
+        console.log(document.querySelector(`.c-to_do__scope#${id}`) + "  sdfasdfsa")
         document.querySelector(`.c-to_do__scope#${id}`).className += " checked"
     }else{
         arrTasks.map(e =>{
@@ -36,22 +37,22 @@ draw = function(){
         if(e.checked == "true"){
             return `
             <div class="c-to_do">
-                <div class="c-to_do__scope checked" id="${eReplaced}">
+                <div class="c-to_do__scope checked" id="id${eReplaced}">
                     <i class="fas fa-times c-to_do__label" onclick="deleteTask('${e.def}')"></i>
                     <p class="c-to_do__def">${e.def}</p>
                 </div>
-                <input type="checkbox" class="c-to_do__check" id="${eReplaced}" onclick="checkboxVerify('${eReplaced}','${e.def}', this)" checked>
+                <input type="checkbox" class="c-to_do__check" id="id${eReplaced}" onclick="checkboxVerify('id${eReplaced}','${e.def}', this)" checked>
                 
             </div>
             `
         }else{
             return `
             <div class="c-to_do">
-                <div class="c-to_do__scope" id="${eReplaced}">
+                <div class="c-to_do__scope" id="id${eReplaced}">
                     <i class="fas fa-times c-to_do__label" onclick="deleteTask('${e.def}')"></i>
                     <p class="c-to_do__def">${e.def}</p>
                 </div>
-                <input type="checkbox" class="c-to_do__check" id="${eReplaced}" onclick="checkboxVerify('${eReplaced}','${e.def}', this)">
+                <input type="checkbox" class="c-to_do__check" id="id${eReplaced}" onclick="checkboxVerify('id${eReplaced}','${e.def}', this)">
                 
             </div>
             `
@@ -60,6 +61,7 @@ draw = function(){
         
     }).join('')
     
+    event.preventDefault()
 }
 
 //ATIVAR/DESATIVAR INPUT
@@ -67,11 +69,13 @@ toggleInput = function(state, element){
     if(state == "on"){
         element.style.display = "initial"
         element.focus()
+        console.log("INPUT ON")
         newTask.style.display = "none"
     }else if(state == "off"){
         element.style.display = "none"
         element.value = ""
         newTask.style.display = "block"
+        console.log("INPUT OFF")
     }
 }
 
@@ -85,10 +89,23 @@ focusOut = function(){
 
 //FUNÇÃO: SALVAR NO LOCAL STORAGE E ATULIZAR CONTEUDO()
 createTask = function(){
-    arrTasks.push({def: input.value, checked: 'false'})
-    localStorage.list = JSON.stringify(arrTasks)
+    let hasRepeated = false
+    arrTasks.forEach(e=>{
+        if(input.value == e.def){ 
+            hasRepeated = true
+        }
+    })
+
+    if(hasRepeated == true){
+        alert("This task already exists")
+        
+    }else{
+        arrTasks.push({def: input.value, checked: 'false'})
+        localStorage.list = JSON.stringify(arrTasks)
+        draw()
+        toggleInput("on", input)
+    }
     input.value = ""
-    draw()
 }
 
 //FUNÇÃO: APAGAR DO LOCAL STORAGE E ATUALIZAT CONTEUDO()
@@ -106,12 +123,28 @@ deleteTask = function(content){
 window.addEventListener("keypress", e =>{
     if(e.key == "Enter"){
         if(input.value != ""){
-            createTask()
+            
+            if(input.value.match(regex) == input.value){
+                createTask()
+            }else{
+                alert("Only letters and numbers are accepted")
+            }
         }else{
             toggleInput("off", input)
         }
     }
 })
+
+//VERIFICAR O TEXTO
+function verifyText(){
+    if(input.value.match(regex) == input.value || input.value == ""){
+        input.style.outline = "1px solid #42474D"
+        
+    }else if(input.value != ""){
+        input.style.outline = "1px solid red"
+        console.log("erro : " + input.value)
+    }
+}
 
 let arrTasks = []
 if(localStorage.list){
